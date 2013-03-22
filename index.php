39c09f1ffbbd4daf25711b55f5edece4af043a6b
hichenming <kyle@mfhui.com>
2013-03-22 09:23:45 +0800

    branch test

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ $all_tags = MongoHelper::getInstance()->find(Otable::DB_LANG, Otable::TABLE_TAGS
 foreach ($all_tags as $key => $value) {
 	$tags[$value['key']] = $value['value'];
 }
-$langs = MongoHelper::getInstance()->find(Otable::DB_LANG, Otable::TABLE_ITEMS, 
+$langs = MongoHelper::getInstance()->find(Otable::DB_LANG, Otable::TABLE_ITEMS,
 	array('la_id'=>array('$gt'=>0)), array( 'limit'=>100, 'sort'=>array('la_id', 1))
 	);
 
 //列出前100条语言
 include __DIR__.'/templates/index.html';
+
+//这是新分支添加的东西。。。
